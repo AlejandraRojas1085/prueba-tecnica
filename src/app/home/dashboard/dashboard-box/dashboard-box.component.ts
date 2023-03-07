445b79b77aac38a17da3529c5cb0dbff0bae5b80
Alejandra <maleja369@gmail.com>
2023-03-07 17:36:56 -0500

    Hotfix: trabajo en lectura de pdf para organizar datos para mostrar informacion

--- src/app/home/dashboard/dashboard-box/dashboard-box.component.ts
+++ src/app/home/dashboard/dashboard-box/dashboard-box.component.ts
@@ -1,3 +1,4 @@
+import { element } from 'protractor';
 
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { ReadCsvService } from "src/app/services/read-csv.service";
@@ -6,6 +7,8 @@ import { MatPaginator } from "@angular/material/paginator";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from '@angular/material/table';
 import * as XLSX from 'xlsx';
+import { resolve } from 'dns';
+import { log } from 'console';
 @Component({
   selector: "app-dashboard-box",
   templateUrl: "./dashboard-box.component.html",
@@ -14,7 +17,7 @@ import * as XLSX from 'xlsx';
 export class DashboardBoxComponent implements OnInit {
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
-  covidData: Array<any> = [];
+  covidData: any;
 
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource: any;
@@ -23,17 +26,36 @@ export class DashboardBoxComponent implements OnInit {
   isLoadingResults = true;
   isRateLimitReached = false;
 
+  listStatesByTotalCovid: Array<any> = [];
+
+  stateMinor: string;
+
+  stateMaxTotalCovid = {
+    name: null,
+    total: 0,
+  }
+
   constructor(
     private readCsvService: ReadCsvService
   ) { }
 
 
   public ngOnInit() {
-    this.getData();
+    this.getState();
   }
 
   async getData() {
-    this.readCsvService.getData().subscribe(async data => {
+    return new Promise((resolve) => {
+      this.readCsvService.getData().subscribe(async data => {
+        resolve(data)
+      });
+    })
+  }
+
+  async dataToJson() {
+    return new Promise(async (resolve) => {
+      let data = await this.getData();
+
       let workBook = null;
       let jsonData = null;
 
@@ -44,23 +66,63 @@ export class DashboardBoxComponent implements OnInit {
         return initial;
       }, {});
 
-      const dataString = JSON.stringify(jsonData);
-      console.log('data ',dataString);
-
-     
-      
-      
-   
-
-         
-     
+      resolve(jsonData);
     });
   }
 
+  async getState() {
+    this.covidData = await this.dataToJson();
+
+    this.covidData.Sheet1.forEach(async element => {
+      let accumulator = await this.accumulatorByRow(element)
+      element.total = accumulator;
+      await this.groupByProvince(element);
+
+    });
+  }
+
+  async accumulatorByRow(item: any) {
+    return new Promise((resolve) => {
+      let accumulator: number = 0;
+
+      for (const total in item) {
+        if (total.includes('/')) {
+          accumulator += item[total];
+        }
+      };
+
+      resolve(accumulator);
+    });
+  }
+
+  groupByProvince(element: any) {
+    const find = this.listStatesByTotalCovid.find(item => item.name === element.Province_State);
+
+    if (!find) {
+      let province = {
+        name: element.Province_State,
+        total: element.total
+      };
+
+      this.listStatesByTotalCovid = [province, ...this.listStatesByTotalCovid];
+    }
+
+    if (find) {
+      find.total += element.total;
+    }
+
+    this.responseQuestions();
+  }
+
+  async responseQuestions() {
+    if (this.listStatesByTotalCovid.length > 0) {
+      let max = this.listStatesByTotalCovid.reduce((a,b)=> {
+      
+        
+      }) 
+        
+    }
+  }
+
   
- 
-  
-
-
-
 } 
